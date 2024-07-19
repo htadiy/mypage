2ac083391fe60045ef7e0e4198db8f714d2ebc3f
htadiy <2676732726@qq.com>
2024-07-19 20:48:07 +0800

    add:首次提交

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="more-content">五年代码，三年BUG</div>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
